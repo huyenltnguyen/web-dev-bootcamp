ba3c9aa0890c1463c8512afd9e5d95d034f78626
huyenltnguyen <huyenltnguyen@outlook.com>
2017-10-17 04:57:49 +0700

    restfulBlogApp final touches

--- RESTful Routing/RESTfulBlogApp/app.js
+++ RESTful Routing/RESTfulBlogApp/app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const expressSanitizer = require('express-sanitizer');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());	// this must go after bodyParser
 app.use(methodOverride('_method'));
 
 // MONGOOSE/MODEL CONFIG
@@ -45,7 +47,9 @@ app.get('/blogs/new', function(req, res) {
 
 // CREATE ROUTE
 app.post('/blogs', function(req, res) {
-	// create blog
+	// create blog	
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+
 	Blog.create(req.body.blog, function(err, newBlog) {
 		if(err) {
 			res.render('new');
@@ -81,6 +85,8 @@ app.get('/blogs/:id/edit', function(req, res) {
 
 // UPDATE ROUTE
 app.put('/blogs/:id', function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
 		if(err) {
 			res.redirect('/blogs');
