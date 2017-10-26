39f76ee7d82b6f24fc685b77cce3deafbd51c8a8
huyenltnguyen <huyenltnguyen@outlook.com>
2017-10-26 15:41:55 +0700

    yelpcamp update and destroy

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,13 +1,14 @@
-const express 			= require('express'),
-			app 					= express(),
-			bodyParser 		= require('body-parser'),
-			mongoose 			= require('mongoose'),
-			passport 			= require('passport'),
-			LocalStrategy = require('passport-local'),
-			Campground 		= require('./models/campground'),
-			Comment 			= require('./models/comment'),
-			User 					= require('./models/user'),
-			seedDB 				= require('./seeds');
+const express 				= require('express'),
+			app 						= express(),
+			bodyParser 			= require('body-parser'),
+			mongoose 				= require('mongoose'),
+			passport 				= require('passport'),
+			LocalStrategy 	= require('passport-local'),
+			methodOverride 	= require('method-override'),
+			Campground 			= require('./models/campground'),
+			Comment 				= require('./models/comment'),
+			User 						= require('./models/user'),
+			seedDB 					= require('./seeds');
 
 // requiring routes
 const campgroundRoutes 	= require('./routes/campgrounds'),
@@ -18,6 +19,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 //seedDB();		// seed the database
 
 // PASSPORT CONFIGURATION
@@ -32,7 +34,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// call this function every single route
+// add currentUser to every single route
 app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
 	next();
--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -42,12 +42,64 @@ router.post('/', isLoggedIn, function(req, res) {
 	});	
 });
 
+// Comments EDIT
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
+	Comment.findById(req.params.comment_id, function(err, foundComment) {
+		if(err) {
+			res.redirect('back');
+		} else {
+			res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+		}
+	});	
+});
+
+// Comments UPDATE
+router.put('/:comment_id', checkCommentOwnership, function(req, res) {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+		if(err) {
+			res.redirect('back');
+		} else {
+			res.redirect(`/campgrounds/${req.params.id}`);
+		}
+	});
+});
+
+// Comments DESTROY
+router.delete('/:comment_id', checkCommentOwnership, function(req, res) {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+		if(err) {
+			res.redirect('back');
+		} else {
+			res.redirect(`/campgrounds/${req.params.id}`);
+		}
+	});
+});
+
 // MIDDLEWARE
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()) {
 		return next();
 	}
 	res.redirect('/login');
+}
+
+function checkCommentOwnership(req, res, next) {
+	if(req.isAuthenticated()) {		
+		Comment.findById(req.params.comment_id, function(err, foundComment) {
+			if(err) {
+				res.redirect('back');
+			} else {
+				// check if user owns the comment
+				if(foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect('back');
+				}				
+			}
+		});
+	} else {
+		res.redirect('back');
+	}
 }
 
 module.exports = router;
